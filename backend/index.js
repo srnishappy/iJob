7f999eba37f1c791217075f26c8ed2273f7a0a6b
Srnishappy <suranatsuwannarat@gmail.com>
2025-04-16 15:19:42 +0700

    setup model&&server

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log('server running port 5000');
+});
