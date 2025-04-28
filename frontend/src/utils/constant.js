cf93fdca7aea3e2d66e14ce1e096bd95cd924e90
Srnishappy <suranatsuwannarat@gmail.com>
2025-04-28 12:39:42 +0700

    add api applyjob

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,2 +1,3 @@
 export const USER_API_END_POINT="http://localhost:5000/api/user"
 export const JOB_API_END_POINT="http://localhost:5000/api/job/"
+export const APPLICATION_API_END_POINT="http://localhost:5000/api/application"
