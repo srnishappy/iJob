d9669e3d77d96c33f48af89eae3739f6f0d40c48
Srnishappy <suranatsuwannarat@gmail.com>
2025-05-03 20:15:02 +0700

    < recruiter

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,12 +3,21 @@ import HeroSection from "./HeroSection"
 import LatestJobs from "./LastestJobs"
 import Footer from "./shared/Footer"
 import Navbar from "./shared/Navbar"
+import { useSelector } from "react-redux"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Home = () => {
     useGetAlljobs()
+    const { user } = useSelector(store => store.auth)
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (user?.role === 'recruiter') {
+            navigate("/admin/companies");
+        }
+    }, []);
     return (
         <div>
-
             <Navbar />
             <HeroSection />
             <LatestJobs />
