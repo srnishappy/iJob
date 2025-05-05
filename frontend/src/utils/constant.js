f5bbc5e96df45642b84dcbb1a125275ac94c4ebf
Srnishappy <suranatsuwannarat@gmail.com>
2025-05-05 21:27:47 +0700

    last fix

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="http://localhost:5000/api/user"
-export const JOB_API_END_POINT="http://localhost:5000/api/job/"
-export const APPLICATION_API_END_POINT="http://localhost:5000/api/application"
-export const COMPANY_API_END_POINT="http://localhost:5000/api/company"
+export const USER_API_END_POINT="https://ijob-hlip.onrender.com/api/user"
+export const JOB_API_END_POINT="https://ijob-hlip.onrender.com/api/job/"
+export const APPLICATION_API_END_POINT="https://ijob-hlip.onrender.com/api/application"
+export const COMPANY_API_END_POINT="https://ijob-hlip.onrender.com/api/company"
